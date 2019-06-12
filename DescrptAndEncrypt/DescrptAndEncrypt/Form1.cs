a732bda834f1b3b739ae162dc5fa41381dd33b2a
piliwang <49442670+piliwang@users.noreply.github.com>
2019-06-12 19:32:28 +0800

    Test
    Just test

--- DescrptAndEncrypt/DescrptAndEncrypt/Form1.cs
+++ DescrptAndEncrypt/DescrptAndEncrypt/Form1.cs
@@ -16,5 +16,10 @@ namespace DescrptAndEncrypt
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world");
+        }
     }
 }
